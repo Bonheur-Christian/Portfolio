eaa5179c0974c0dfa1f7e4d4796f3fbdc25dbaad
M.Bonheur <mbonheurc23@gmail.com>
2024-10-13 18:59:13 +0200

    updated version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,19 @@ import LandingBar from "../components/LandingBar";
 import Projects from "../components/Projects";
 import Testimonials from "../components/Testimonial";
 
-
-function Home(){
-    return(
-        <div className="space-y-2 w-[80%] mx-auto">
-            <LandingBar/>
-            <Intro/>
-            <div className="bg-[#1F1E1E] h-24 -ms-48 -me-48"></div>
-            <About/>
-            <Testimonials/>
-            <Projects/>
-            <Contacts/> 
-            <Footer/>
-        </div>
-    )
+function Home() {
+  return (
+    <div className="space-y-2 w-[80%] mx-auto">
+      <LandingBar />
+      <Intro />
+      <div className="bg-[#1F1E1E] h-24 -ms-48 -me-48"></div>
+      <About />
+      <Testimonials />
+      <Projects />
+      <Contacts />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
