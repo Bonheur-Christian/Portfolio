16f6391f88046589cea29168c23ff5136ac471f2
M.Bonheur <mbonheurc23@gmail.com>
2024-10-17 21:23:44 +0200

    updated version

--- src/components/LandingBar.jsx
+++ src/components/LandingBar.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiDownload } from "react-icons/fi";
 import { IoMdMenu } from "react-icons/io";
 import { IoCloseSharp } from "react-icons/io5";
 
 function LandingBar() {
   const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const Links = [
     { link: "Home", dest: "/" },
     { link: "About", dest: "#about" },
@@ -15,6 +31,13 @@ function LandingBar() {
   const toggleMenu = () => {
     setOpen(!open);
   };
+
+  useEffect(() => {
+    if (scrolled) {
+      setOpen(false);
+    }
+  }, [scrolled]);
+
   return (
     <div>
       <nav className="text-white flex justify-between lg:px-12 sm:px-4 py-12">
@@ -25,7 +48,6 @@ function LandingBar() {
               alt="Personal Logo"
               width={180}
               height={80}
-              className=""
             />
           </a>
         </div>
@@ -33,16 +55,16 @@ function LandingBar() {
           className={`${
             open
               ? "pt-20 flex flex-col space-y-4 me-[-8rem]"
-              : "lg:flex items-center gap-24 sm:hidden"
+              : "sm:hidden"
           }`}
         >
-          {Links.map((item, index) => (
-            <ul key={index}>
-              <a href={item.dest}>
-                <li className="underline-animation">{item.link}</li>
-              </a>
-            </ul>
-          ))}
+          <ul className="lg:flex sm:space-y-4 items-center gap-24">
+            {Links.map((item, index) => (
+              <li className="underline-animation" key={index}>
+                <a href={item.dest}>{item.link}</a>
+              </li>
+            ))}
+          </ul>
           <button className="border border-3 text-xl border-[#FF4900] font-semibold rounded-full px-4 py-2 hover:bg-[#FF4900] lg:flex items-center gap-4 sm:hidden md:flex">
             <FiDownload />
             Download CV
