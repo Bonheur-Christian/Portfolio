9c0eb83bf80ec8ca762e0d1f017543ee9c450fa7
M.Bonheur <mbonheurc23@gmail.com>
2024-12-24 11:41:35 +0200

    first version of the portifolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,7 @@ function Footer() {
   return (
     <div className="flex py-6 justify-center lg:mx-auto sm:mx-2">
       <p className="flex items-center font-medium text-md lg:text-gray-700 sm:text-gray-400 text-center">
-        Developed With
-        <img src="/svg/flower.svg" alt="Love" width={30} className="sm:hidden lg:block" />
-        , Courage And Commitment By Bonheur.
+        Developed With Courage And Commitment By Bonheur.
       </p>
     </div>
   );
